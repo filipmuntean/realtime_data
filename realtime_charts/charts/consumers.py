3fb9f7f90edf737f8a2fad0f6919bcc9e7ff4d7b
datamaking <datamaking.training@gmail.com>
2022-07-08 18:34:40 +0530

    adding chart.js with redis integration

--- realtime_charts/charts/consumers.py
+++ realtime_charts/charts/consumers.py
@@ -1,14 +1,14 @@
 import json
 from random import randint
-from time import sleep
+from asyncio import sleep
 
-from channels.generic.websocket import WebsocketConsumer
+from channels.generic.websocket import AsyncWebsocketConsumer
 
 
-class ChartsConsumer(WebsocketConsumer):
-    def connect(self):
-        self.accept()
+class ChartsConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        await self.accept()
 
         for i in range(1000):
-            self.send(json.dumps({'value': randint(0, 25)}))
-            sleep(1)
+            await self.send(json.dumps({'value': randint(0, 25)}))
+            await sleep(5)
--- realtime_charts/static/main.js
+++ realtime_charts/static/main.js
@@ -1,3 +1,50 @@
+const ctx = document.getElementById('myChart').getContext('2d');
+
+var chartsData = {
+    type: 'bar',
+    data: {
+        labels: ['Visa', 'Master', 'Maestro', 'American Express', 'Cirrus', 'PayPal'],
+        datasets: [{
+            label: 'No. of Orders',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1,
+            barPercentage: 0.5
+        }]
+    },
+    options: {
+      scales: {
+          x: {
+          title: {
+            display: true,
+            text: 'Card Type',
+            font: {
+                size: 25
+              }
+            }
+          }
+      }
+    }
+}
+
+const myChart = new Chart(ctx, chartsData);
+
+
 console.log('Hello World!');
 
 var socket = new WebSocket('ws://localhost:8000/ws/charts/')
@@ -8,6 +55,12 @@ socket.onmessage = function(e){
     console.log(djangoData);
     //alert(djangoData);
     //alert(djangoData.value);
+
+    newChartsData = chartsData.data.datasets[0].data;
+    newChartsData.shift();
+    newChartsData.push(djangoData.value);
+    chartsData.data.datasets[0].data = newChartsData;
+    myChart.update()
 
     h1Element = document.getElementById("app")
 
